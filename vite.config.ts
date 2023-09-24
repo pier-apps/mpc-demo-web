706b66de38247dcbe038a3b2de0cd9600012bcca
oleh <oleh@pierwallet.com>
2023-09-25 01:41:13 +0300

    build: single file

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
+import { viteSingleFile } from "vite-plugin-singlefile";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), viteSingleFile()],
   optimizeDeps: {
     exclude: ["@pier-wallet/mpc-ecdsa-wasm"],
   },
